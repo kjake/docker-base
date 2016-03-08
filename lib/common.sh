53ff9040eac28e83da429bc9acd1deb74cd29880
Josh King <jking@chambana.net>
2016-03-08 14:48:38 -0500

    Add additional convenience functions to lib/common.sh

--- lib/common.sh
+++ lib/common.sh
@@ -21,3 +21,16 @@ function MSG() {
   local n='\033[0m'
   echo -e "${i}${r} $1$n" 
 }
+
+## Prettify error messages
+function ERR() {
+  local i='\033[1;32m(\033[0;32m(\033[1;30m(\033[mi\033[1;30m)\033[0;32m)\033[1;32m)\033[m'
+  local r='\033[0;31m'
+  local n='\033[0m'
+  echo >&2 -e "${i}${r} $1$n" 
+}
+
+## Check for existence of an executable and abort if not found.
+function CHECK_BIN() {
+	hash "$1" 2>/dev/null || { ERR "Program '$1' not found.  Aborting."; exit 1; }
+}
